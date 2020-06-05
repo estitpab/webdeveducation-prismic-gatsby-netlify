033cfa74e949be9e17e90317e6d6272178f0e78e
Baptiste <baptiste@enviedefraise.fr>
2020-06-05 23:02:21 +0200

    Adding pricelist infos

--- src/components/priceItem.js
+++ src/components/priceItem.js
@@ -1,8 +1,24 @@
 import React from "react"
+import { RichText } from "prismic-reactjs"
+import styled from "styled-components"
 
-const priceItem = () => {
+const PriceItemWrapper = styled.div`
+  flex-grow:1;
+  flex-basis:0;
+  margin: 0 10px;
+  
+`
+
+
+const priceItem = ({ title, description, price, type }) => {
   return (
-    <div>Price Item</div>
+    <PriceItemWrapper>
+      <RichText render={title}/>
+      <div className="price">
+        {price} <span className="duration">/month</span>
+      </div>
+      <RichText render={description}/>
+    </PriceItemWrapper>
   )
 }
 
--- src/components/priceList.js
+++ src/components/priceList.js
@@ -6,6 +6,10 @@ import PriceItem from "./priceItem"
 const PriceListWrapper = styled.section`
   max-width: 800px;
   margin: 0 auto;
+  
+  div:last-child{
+    display: flex;
+  }
 
 `
 
@@ -13,13 +17,19 @@ const PriceList = ({ title, prices }) => {
   return (
     <PriceListWrapper>
       <RichText render={title}/>
-      {prices.map((price, i) => {
-        return (
-          <PriceItem
-            key={i}
-          />
-        )
-      })}
+      <div>
+        {prices.map((price, i) => {
+          return (
+            <PriceItem
+              key={i}
+              title={price.price_list_title}
+              description={price.price_list_description}
+              price={price.price_per_month}
+              type={price.price_type}
+            />
+          )
+        })}
+      </div>
     </PriceListWrapper>
   )
 
